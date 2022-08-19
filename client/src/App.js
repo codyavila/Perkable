22b63abece7cf4bafd8c759c77a79f37d8e884f8
sanderscody26 <sanderscody26@gmail.com>
2022-08-19 12:16:43 -0400

    Changed Primary and Secondary colors

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,20 @@ const App = () => {
   const theme = createTheme({
     typography: {
       fontFamily: ['Oswald', 'sans-serif', 'Poppins', 'sands-serif'].join(',')
+    },
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#3f50b5',
+        dark: '#002884',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000'
+      }
     }
   })
 

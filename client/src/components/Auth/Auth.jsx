efc7b52caa96ff1c1755c4f8813c557eb6f19df2
sanderscody26 <sanderscody26@gmail.com>
2022-08-19 18:39:28 -0400

    Setup Redux to make a backend call for Auth

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -13,13 +13,23 @@ import jwt_decode from 'jwt-decode'
 import { useDispatch } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 
+import { signin, signup } from '../../actions/auth'
 import Input from './Input'
 import useStyles from './styles'
+
+const initialState = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: ''
+}
 
 const Auth = () => {
   const classes = useStyles()
   const [showPassword, setShowPassword] = useState(false)
   const [isSignup, setIsSignup] = useState(false)
+  const [formData, setFormData] = useState(initialState)
 
   const dispatch = useDispatch()
   const history = useHistory()
@@ -39,9 +49,19 @@ const Auth = () => {
   const handleShowPassword = () =>
     setShowPassword((prevShowPassword) => !prevShowPassword)
 
-  const handleSubmit = () => {}
+  const handleSubmit = (e) => {
+    e.preventDefault()
 
-  const handleChange = () => {}
+    if (isSignup) {
+      dispatch(signup(formData, history))
+    } else {
+      dispatch(signin(formData, history))
+    }
+  }
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   const switchMode = () => {
     setIsSignup((prevIsSignup) => !prevIsSignup)
